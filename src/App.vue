4f2c79260eeeb1a68e575358ad7f8aca3b860d15
Lenni009 <guentherhanslphdyt@gmail.com>
2023-07-12 22:07:42 +0200

    initial rewrite attempt

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Navbar from 'src/components/navbar.vue';
+import PageHeader from 'src/components/page-header.vue';
+
+
+</script>
+
+<template>
+		<navbar />
+		<page-header />
+
+
+</template>
---
+++ src/components/helper-buttons.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import HelpDialog from './dialog/help-dialog.vue';
+
+const isModalOpen = ref(false);
+const helpDialog = ref(null);
+
+const switchModalVisibility = () => isModalOpen.value = !isModalOpen.value;
+
+</script>
+
+<template>
+	<help-dialog :open="isModalOpen" ref="helpDialog" @close-modal="switchModalVisibility" />
+	<div class="helper-buttons">
+		<button id="explainBtn" class="button" @click="switchModalVisibility">
+			How to use
+		</button>
+		<button></button>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.helper-buttons {
+	margin-block-end: 1em;
+
+	button {
+		margin-inline-end: 1em;
+	}
+}
+</style>
